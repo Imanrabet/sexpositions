17e49feed4c0fa8d23e627406265873fd1924528
oseitutunelson <oseitutunelson11@gmail.com>
2024-05-19 10:25:52 +0000

    added .next to gitignore

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -188,7 +188,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5ed3176e09ba18c9"; }
+/******/ 		__webpack_require__.h = function() { return "d2bab8b884863f8e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
